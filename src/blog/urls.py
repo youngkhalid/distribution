4ad0861de9bbbdcfcf19f5b87169d1b1842e1966
Brian Karanja <youngkhalifah98@gmail.com>
2019-08-12 19:06:35 +0300

    Initial commit

---
+++ src/blog/urls.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+
+
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.urls import path
+from . views import index, blog, post
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index),
+    path('blog/', blog),
+    path('post/', post),
+    
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
