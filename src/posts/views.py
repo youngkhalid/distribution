63e7ac390367cddae2230cbcc4fe854bd0e76132
Brian Karanja <youngkhalifah98@gmail.com>
2019-08-19 19:28:03 +0300

    Pagination done

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 from marketing.models import Signup
 
@@ -13,15 +14,31 @@ def index(request):
         new_signup_email = email
 
         new_signup.save()
-        
+
     context = {
-        'object_List': featured,
+        'object_list': featured,
         'latest': latest
     }
     return render(request, 'index.html', context)
 
 def blog(request):
-    return render(request, 'blog.html', {})
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 1)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+     # paginating the pages
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
+
+    context = {
+        'queryset': paginated_queryset,
+        'page_request_var': page_request_var
+    }
+    return render(request, 'blog.html', context)
 
 def post(request):
     return render(request, 'post.html', {})
