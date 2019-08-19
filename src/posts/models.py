a1929ed467da5bd1cd7c9b7ea7f7ed291aa24da8
Brian Karanja <youngkhalifah98@gmail.com>
2019-08-19 19:48:28 +0300

    sidebar put to logic

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.urls import reverse
 
 User = get_user_model()
 
@@ -25,6 +25,7 @@ class Post(models.Model):
     overview = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     comment_count = models.IntegerField(default=0)
+    view_count = models.IntegerField(default=0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     thumbnail = models.ImageField()
     categories = models.ManyToManyField(Category)
@@ -32,4 +33,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        
+        return reverse('post_detail', kwargs={'id': self.id})
 
--- src/posts/views.py
+++ src/posts/views.py
@@ -22,6 +22,7 @@ def index(request):
     return render(request, 'index.html', context)
 
 def blog(request):
+    most_recent = Post.objects.order_by('-timestamp')[:3]
     post_list = Post.objects.all()
     paginator = Paginator(post_list, 1)
     page_request_var = 'page'
@@ -36,9 +37,10 @@ def blog(request):
 
     context = {
         'queryset': paginated_queryset,
-        'page_request_var': page_request_var
+        'page_request_var': page_request_var,
+        'most_recent': most_recent
     }
     return render(request, 'blog.html', context)
 
-def post(request):
+def post(request, id):
     return render(request, 'post.html', {})
