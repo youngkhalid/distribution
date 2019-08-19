c2dac8fd7a7d31cdce4be03a766522318d991f41
Brian Karanja <youngkhalifah98@gmail.com>
2019-08-19 13:51:23 +0300

    featured view implemented

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from .models import Post
 def index(request):
-    return render(request, 'index.html', {})
+    featured = Post.objects.filter(featured=True)
+    latest = Post.objects.order_by('-timestamp')[0:3]
+    context = {
+        'object_List': featured,
+        'latest': latest
+    }
+    return render(request, 'index.html', context)
 
 def blog(request):
     return render(request, 'blog.html', {})
